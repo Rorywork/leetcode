4dd6546aa1a178f97569b8d00aea33af8e17b276
Rory <rory.lindsay@pulselive.com>
2022-07-14 11:52:56 +0100

    (solved): two-sum js one pass with hashmap

---
+++ solutions/two-sum-3.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let hashmap = {};
+    for(let i=0; i<=nums.length; i++) {
+        complement = target - nums[i];
+        if ( complement in hashmap ) {
+            return [i, hashmap[complement]]
+        }
+        hashmap[nums[i]] = i;
+    }
+};
+// @lc code=end
+
